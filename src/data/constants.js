b4b616d015b4d1b77350c6c5c9d70ceae4dd3b07
MEshareebRajput <muhammadeshareeb986@gmail.com>
2024-10-20 16:21:17 -0700

    Portfolio showcasing projects and skills. 💻
    This portfolio is built with React.js 🚀, showcasing my projects 🛠️, skills 💻, and experiences as a MERN Stack Developer. It demonstrates my ability to create responsive and dynamic web applications 🌟.

---
+++ src/data/constants.js
@@ -0,0 +1,259 @@
+export const Bio = {
+  name: "Muhammad Eshareeb Rajput",
+  roles: [
+    "MERN Stack Developer",
+    "Android Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a Junior MERN Stack Developer with a passion for creating user-friendly applications and continuously enhancing my skills in both frontend and backend development.",
+  github: "https://github.com/MEshareebRajput",
+  resume:
+    "https://drive.google.com/file/d/14xPSuBjqmIQY1af2PXu0LbvI40rgtnuB/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/muhammadeshareeb986/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://youteam.io/blog/wp-content/uploads/2022/04/expressjs_logo.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },{
+    title: "Android",
+    skills: [
+      {
+        name: "Android Studio",
+        image: "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "React Native",
+        image: "https://reactnative.dev/img/header_logo.svg",
+      },
+      {
+        name: "Expo",
+        image: "https://www.cdnlogo.com/logos/e/72/expo-go-app.svg",
+      },
+    ],
+  },  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://w7.pngwing.com/pngs/28/245/png-transparent-postman-hd-logo-thumbnail.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://w7.pngwing.com/pngs/911/515/png-transparent-figma-logo-brand-logos-brands-in-colors-icon-thumbnail.png",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://media.licdn.com/dms/image/v2/D5622AQHrdl89ZDOIMQ/feedshare-shrink_800/feedshare-shrink_800/0/1713529716396?e=2147483647&v=beta&t=yxv2X9ckRz0jeOHO8cHjJAiPUv3K4klmY73Tq69UH0Y",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://w7.pngwing.com/pngs/436/888/png-transparent-vercel-hd-logo-thumbnail.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzcejFcWTbn1ROhWXftBjLOPUQHecn1ASa6g&s",
+    role: "MERN Stack Developer Intern",
+    company: "Eflex Solution Pvt. Ltd.",
+    date: "April 2024 - Sep 2024",
+    desc: "As a MERN Stack Intern at eFlex Solutions, I contributed to developing and optimizing web applications while collaborating on best practices in software development.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "ReactJS",
+      "NodeJS",
+      "MongoDB",
+      "ExpreesJS",
+      "Firebase",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0nLspHKKBrfm_dbO4100qZJCCeay4hUsFrQ&s",
+    school: "Govt. Degree Boys College 11-A, North Karachi.",
+    date: "Jan 2024 - Dec 2025",
+    grade: "Wating For Result",
+    desc: "I am currently enrolled in college, pursuing a degree in Pre-Engineering. This program equips me with essential skills in mathematics and science, preparing me for further studies in engineering.",
+    degree: "Intermediate (FSc) in Pre-Engineering, Pakistan",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-cXFuavrc6SQ1s7DJvs55FQ-BF0bqYSM-iw&s",
+    school: "S.M.I.T (Saylani Mass I.T Training)",
+    date: "2023 - 2024",
+    grade: "100%",
+    desc: "I have completed my course in Web Development at S.M.I.T (Saylani Mass I.T Training) , focusing on MERN Stack Development. This program equipped me with essential skills in MongoDB, Express.js, React.js, and Node.js, enabling me to build dynamic and efficient web applications.",
+    degree: "Diploma in Web Development - MERN Stack Development",
+  },
+  {
+    id: 2,
+    img: "https://avatars.githubusercontent.com/u/81553050?v=4",
+    school: "Jawan Pakistan",
+    date: "Mar 2023 - Nov 2023",
+    grade: "100%",
+    desc: "I have completed my course in Web Development, specializing in Frontend Development at Jawan Pakistan. This program equipped me with essential skills in HTML, CSS, JavaScript, and React.js, enabling me to create dynamic and responsive user interfaces for web applications.",
+    degree: "Diploma in Web Development - Frontend Development",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-cXFuavrc6SQ1s7DJvs55FQ-BF0bqYSM-iw&s",
+    school: "S.M.I.T (Saylani Mass I.T Training)",
+    date: "October 2024 - Present",
+    grade: "Present",
+    desc: "I am currently pursuing a course in Mobile App Development, focusing on React Native and Expo. This program is equipping me with the skills to create cross-platform mobile applications that provide seamless user experiences across various devices.",
+    degree: "Diploma in Mobile App Development - React Native",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Virtual-Assistant-OD",
+    date: "Oct 2024 - Two Day",
+    description:
+      "OD is a virtual assistant designed to assist users with a wide range of tasks, providing quick responses and intelligent support. Utilizing advanced technologies such as natural language processing and machine learning, OD can engage in conversations, perform web searches, manage schedules, and respond to various user commands, making it a reliable tool for enhancing productivity and streamlining daily activities.",
+    image:
+      "https://virtual-assistant-od.vercel.app/image/logo.jpg",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "https://github.com/MEshareebRajput/Virtual-Assistant-OD.git",
+    webapp: "https://virtual-assistant-od.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Apple-Store-Website",
+    date: "Oct 2024 - One Day",
+    description:
+      "The Apple Store website is a dynamic e-commerce platform that showcases Apple's latest products, including iPhones, iPads, Macs, and accessories. Designed with a user-friendly interface, it offers seamless navigation, detailed product descriptions, and customer reviews. The site features secure payment options, personalized recommendations, and exclusive deals, providing customers with a convenient shopping experience that reflects Apple's commitment to innovation and quality.",
+    image:
+      "https://apple-store-website-rosy.vercel.app/image/Capture.PNG",
+    tags: ["HTML", "CSS","BootStrap"],
+    category: "web app",
+    github: "https://github.com/MEshareebRajput/Apple-Store-Website.git",
+    webapp: "https://apple-store-website-rosy.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Digital-Clock",
+    date: "Oct 2024 - One Day",
+    description:
+      "The Digital Clock is a simple yet functional application that displays the current time in a sleek and modern interface. It features real-time updates, allowing users to easily track the time. The app can be customized with different themes and formats, providing an engaging user experience. Ideal for anyone looking for a convenient way to keep track of time, this digital clock can also serve as a stylish addition to any desktop or mobile device.",
+    image:
+      "https://toppng.com/uploads/preview/digital-timer-download-transparent-png-image-digital-clock-clipart-transparent-11563206619rakd503k5m.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "https://github.com/MEshareebRajput/Digital-Clock.git",
+    webapp: "https://digital-clock-xi-flame.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "React Native",
+    date: "In Future",
+    description:
+      "I am currently learning React Native, and I plan to work on several projects in the future.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1400/1*PaCLV0c8MhG1Zj8u9R1M3Q.png",
+    tags: ["React Natvie", "Expo"],
+    category: "android app",
+    github: "-------",
+    webapp: "--------",
+  },
+];
+
